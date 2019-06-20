4ae0dc2495b9a6e6fe27744fe3a0f0938f7875f8
Satoshi Yamamoto <satoshiyamamoto@me.com>
2019-06-20 23:11:19 +0900

    Polished.

--- src/main/java/com/example/demo/Todo.java
+++ src/main/java/com/example/demo/Todo.java
@@ -1,18 +1,19 @@
 package com.example.demo;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "todos")
 public class Todo {
-	@Id @GeneratedValue
-	private Long id;
-	private String title;
-	private boolean completed;
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String title;
+  private boolean completed;
 }
--- src/main/resources/db/migration/V1__Create_todos_table.sql
+++ src/main/resources/db/migration/V1__Create_todos_table.sql
@@ -1,7 +1,9 @@
-CREATE TABLE todos (
-    id INT PRIMARY KEY,
-    title VARCHAR(255),
+CREATE TABLE todos
+(
+    id        INT PRIMARY KEY,
+    title     VARCHAR(255),
     completed BOOLEAN
 );
 
-INSERT INTO todos VALUES (1, 'Build test environment.', false);
+INSERT INTO todos
+VALUES (1, 'Build test environment.', false);
